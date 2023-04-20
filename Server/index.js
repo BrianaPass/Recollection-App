28d40fbdb5c2b8e9732dc6ed91a5d2f586d87e74
BrianaPass <116980413+BrianaPass@users.noreply.github.com>
2023-04-20 19:48:55 -0400

    updated

--- Server/index.js
+++ Server/index.js
@@ -3,15 +3,20 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/post.js';
+
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extened: true}));
-app.use(bodyParser.urlencoded({ limit: "30mb", extened: true}));
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://Recollectionapp:Recollectionapp123@cluster0.nrijuml.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT|| 9999;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
+  
