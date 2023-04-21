936ccf9832b5ba2aa7a5f962f081575159daf27b
BrianaPass <116980413+BrianaPass@users.noreply.github.com>
2023-04-20 23:46:17 -0400

    homePage

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 import recollection from './images/recollection.png';
 
 const App = () => {
@@ -10,6 +12,17 @@ const App = () => {
                 <Typography varaint="h1" align="center">Recollection</Typography>
                 <img src={recollection} alt="recollection" height="300" />
             </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch spacing={3}">
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}></Grid>
+                        <Form />
+                    </Grid>
+                </Container>
+            </Grow>
         </Container>
     );
 }
