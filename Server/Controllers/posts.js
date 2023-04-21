b96e8efec156ebc56dc82d91ee0fd8a88b19b2eb
BrianaPass <116980413+BrianaPass@users.noreply.github.com>
2023-04-20 23:28:38 -0400

    homepage

--- Server/Controllers/posts.js
+++ Server/Controllers/posts.js
@@ -4,14 +4,25 @@
     try {
         const postMessage = await PostMessage.find();
 
-        res.status(200).json(postMessages);
+        res.status(200).json(postMessage);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
 
-}
+};
 
-export const createPosts = (req, res) => {
-    res.send('Post Creation');
-}
+export const createPosts = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage(post);
+
+    try { 
+       await newPost.save();
+
+       res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+        
+    }
+};
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import recollection from './images/recollection.png';
 
 const App = () => {
     return (
-        <div>
-            <h1>APP</h1>
-        </div>
-    )
+        <Container maxwidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography varaint="h1" align="center">Recollection</Typography>
+                <img src={recollection} alt="recollection" height="300" />
+            </AppBar>
+        </Container>
+    );
 }
 export default App;
