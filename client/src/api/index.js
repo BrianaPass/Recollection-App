8566d619476d7e9b93b67e2ca35ba7ec6e87ec91
BrianaPass <116980413+BrianaPass@users.noreply.github.com>
2023-04-23 18:44:13 -0400

    posts can be saved in database

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,5 +2,8 @@ import axios from 'axios';
 
 const url = 'http://localhost:9999/posts';
 
- export const fetchPosts = () => axios.get(url);
+export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost);
+
+
  
