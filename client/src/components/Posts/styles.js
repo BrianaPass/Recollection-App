30213af3d78e09cb780fb56e098a4d92ad226ef6
MauritzMaj <mauritz.majelyne@hotmail.com>
2023-04-27 19:51:43 -0400

    updated mui/styles import

--- client/src/components/Posts/styles.js
+++ client/src/components/Posts/styles.js
@@ -1,12 +1,13 @@
 import { makeStyles } from "@mui/styles";
+import { useTheme } from '@mui/material/styles';
 
-export default makeStyles((theme) => ({
+export default makeStyles(() => ({
     mainContainer: {
       display: 'flex',
       alignItems: 'center',
     },
     smMargin: {
-      margin: theme.spacing(1),
+      margin: useTheme.spacing(1),
     },
     actionDiv: {
       textAlign: 'center',
